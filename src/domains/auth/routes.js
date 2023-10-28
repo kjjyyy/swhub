a64493ddc4e386fd05fa5a7226ac4d372bf6b8ea
Seunghyeok Lee <seunghyeok.tech@gmail.com>
2023-10-28 22:22:59 +0900

    add: authRouter 생성

---
+++ src/domains/auth/routes.js
@@ -0,0 +1,19 @@
+import express from "express";
+
+const authRouter = express.Router();
+
+authRouter
+  .route("/join")
+  .get((req, res, next) => res.send("join 페이지"))
+  .post((req, res, next) => res.send("join 성공"));
+authRouter
+  .route("/login")
+  .get((req, res, next) => res.send("login 페이지"))
+  .post((req, res, next) => res.send("login 성공"));
+authRouter.route("/logout").post((req, res, next) => res.send("logout 성공"));
+authRouter
+  .route("/change-password")
+  .get((req, res, next) => res.send("비밀번호 변경 페이지"))
+  .put((req, res, next) => res.send("비밀번호 변경 성공"));
+
+export default authRouter;
--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,10 @@
 import express from "express";
 import userRouter from "./domains/users/routes";
+import authRouter from "./domains/auth/routes";
 
 const router = express.Router();
 
+router.use("/auth", authRouter);
 router.use("/users", userRouter);
 
 export default router;
